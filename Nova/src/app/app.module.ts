5baf6eabac98a5ddd86cf435a7b5b2c981beff51
ajdixon30 <adam.dixon30@gmail.com>
2021-11-23 12:31:34 -0500

    Added Search Icon and installed FontAwesome Modules

--- Nova/src/app/app.module.ts
+++ Nova/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { FilterComponent } from './components/filter/filter.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
