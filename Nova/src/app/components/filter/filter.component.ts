1474f270b3748ff3e8e5577351f4483c13f6e435
ajdixon30 <adam.dixon30@gmail.com>
2021-12-02 15:09:33 -0500

    Working on Product Details Page

--- Nova/src/app/components/filter/filter.component.ts
+++ Nova/src/app/components/filter/filter.component.ts
@@ -38,7 +38,7 @@ export class FilterComponent implements OnInit, OnDestroy {
   message!: String;
   subscription!: Subscription;
   sent!: String;
-  constructor(_productsService: ProductsService, private data: DataService) {
+  constructor(_productsService: ProductsService, private data: DataService, _rawg:RawgService, _router:Router) {
     this.productsService = _productsService;
     this.rawg = _rawg;
     this.router = _router;
