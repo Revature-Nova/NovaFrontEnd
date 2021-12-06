27c9a129485017199ff202d4d93f750ec8e7c345
Emmanuel <emmanuel_tejeda@my.uri.edu>
2021-12-06 11:46:17 -0500

    working devOps

--- Nova/src/app/components/user-profile/user-profile.component.ts
+++ Nova/src/app/components/user-profile/user-profile.component.ts
@@ -3,39 +3,39 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { UserProfileService } from 'src/app/services/user-profile.service';
 import { HttpClient } from '@angular/common/http';
 
-
-
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+  styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
-
   form: FormGroup | any;
 
-  constructor(private formBuilder: FormBuilder, private profile: UserProfileService, private http: HttpClient ) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private profile: UserProfileService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
-
     this.form = this.formBuilder.group({
-      username: sessionStorage.getItem("username"),
+      username: sessionStorage.getItem('username'),
       email: '',
       state: '',
       favoriteGenre: '',
-      message: ''
-    })
+      message: '',
+    });
   }
 
   submit(): void {
     console.log(this.form.getRawValue());
     this.http
-      .post('http://18.212.102.32:8082/user-service/Nova/user/profile', this.form.getRawValue())
+      .post(
+        'http://18.212.102.32:8082/user-service/Nova/user/profile/set',
+        this.form.getRawValue()
+      )
       .subscribe((res) => {
         console.log(res);
       });
   }
-
-
-
 }
